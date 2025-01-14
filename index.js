bc155fb9fe97dcc7224c2c18802eff3db43df936
213Hemant <2022.hemant.satam@ves.ac.in>
2025-01-14 12:28:06 +0530

    Added firebase auth and gitignore

--- index.js
+++ index.js
@@ -2,24 +2,9 @@
 document.getElementById("signup-link").addEventListener("click", function () {
     document.getElementById("login-form").classList.add("hidden");
     document.getElementById("signup-form").classList.remove("hidden");
-    document.getElementById("login-tab").classList.remove("bg-gray-800", "text-white");
-    document.getElementById("login-tab").classList.add("bg-gray-200", "text-gray-600");
-    document.getElementById("signup-tab").classList.add("bg-gray-800", "text-white");
-    document.getElementById("signup-tab").classList.remove("bg-gray-200", "text-gray-600");
+   
 });
-
 document.getElementById("login-link").addEventListener("click", function () {
     document.getElementById("signup-form").classList.add("hidden");
     document.getElementById("login-form").classList.remove("hidden");
-    document.getElementById("signup-tab").classList.remove("bg-gray-800", "text-white");
-    document.getElementById("signup-tab").classList.add("bg-gray-200", "text-gray-600");
-    document.getElementById("login-tab").classList.add("bg-gray-800", "text-white");
-    document.getElementById("login-tab").classList.remove("bg-gray-200", "text-gray-600");
-});
-
-// Simulate successful login (Show notes section)
-document.querySelector(".btn-custom.w-full.py-3").addEventListener("click", function () {
-    document.getElementById("login-form").classList.add("hidden");
-    document.getElementById("signup-form").classList.add("hidden");
-    document.getElementById("notes-section").classList.remove("hidden");
 });
